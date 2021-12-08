17db438eeefe0d99845acb66ea31f3ddae7ab6b8
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-08 22:15:40 +0800

    update

--- server.js
+++ server.js
@@ -5,24 +5,15 @@ const pool = require("./database");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 
-//process.env.PORT
-//process.env.NODE_ENV => production or undefined
-
 //middleware
 app.use(cors());
-app.use(express.json()); // => allows us to access the req.body
-
-// app.use(express.static(path.join(__dirname, "client/build")));
-// app.use(express.static("./client/build")); => for demonstration
+app.use(express.json()); 
 
 if (process.env.NODE_ENV === "production") {
   //server static content
   //npm run build
   app.use(express.static(path.join(__dirname, "client/build")));
 }
-
-console.log(__dirname);
-console.log(path.join(__dirname, "client/build"));
 
 //ROUTES//
 
