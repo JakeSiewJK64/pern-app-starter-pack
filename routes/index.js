5826376e05cc06d713f526f8d54e9c32fd371753
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-08 23:40:24 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render(path.join(__dirname, "../client/public", "index.html"));
 });
 
 router.get('/api', function (req, res, next) {
-    res.render({ message: 'Well Done' });
+    res.send({ message: 'Well Done' });
 });
 
 module.exports = router;
