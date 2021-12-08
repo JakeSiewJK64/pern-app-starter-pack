e8415662f3886d5fad3efb5fbbdbb6e379460451
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-08 21:04:02 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
 const app = express();
-const path = require('path');
+const cors = require("cors");
+const pool = require("./database");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
-const pool = require('./database');
 
+//process.env.PORT
+//process.env.NODE_ENV => production or undefined
+
+//middleware
 app.use(cors());
 app.use(express.json()); // => allows us to access the req.body
 
+// app.use(express.static(path.join(__dirname, "client/build")));
+// app.use(express.static("./client/build")); => for demonstration
+
 if (process.env.NODE_ENV === "production") {
+  //server static content
+  //npm run build
   app.use(express.static(path.join(__dirname, "client/build")));
 }
+
+console.log(__dirname);
+console.log(path.join(__dirname, "client/build"));
+
+//ROUTES//
+
+//get all Todos
 
 app.get('/api/customers', cors(), (req, res) => {
   const customers = [
