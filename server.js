5450904060cf4a714bc492c1db15bab7c700bcc5
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-08 20:47:03 +0800

    update server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
-const port = 5000;
-
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const pool = require('./database');
 
-app.listen(port, () => `Server running on port ${port}`);
+app.use(cors());
+app.use(express.json()); // => allows us to access the req.body
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is starting on port ${PORT}`);
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 app.get('/api/customers', cors(), (req, res) => {
   const customers = [
