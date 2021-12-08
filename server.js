a70c58e94c73f005318aac85ffe43ca52a896c77
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-08 22:42:06 +0800

    update scripts

--- server.js
+++ server.js
@@ -7,17 +7,14 @@ const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  //server static content
-  //npm run build
-  app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV-- - 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  })
 }
-
-//ROUTES//
-
-//get all Todos
 
 app.get('/api/customers', cors(), (req, res) => {
   const customers = [
