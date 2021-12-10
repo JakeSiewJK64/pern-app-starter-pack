7da7ccbb18ad6c3d026fecf66662691d507bb887
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-10 20:36:26 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,20 +1,13 @@
 const Pool = require('pg').Pool;
-// const pool = new Pool({
-//     user: "dlndhisnezlfir",
-//     host: "ec2-54-161-189-150.compute-1.amazonaws.com",
-//     password: "97dc6c2cce0633cf9f147f4e3038d106401edd808a33ed18fd008a6d7e59dd0e",
-//     port: 5432,
-//     database: "d9ie64bk2fabqo",
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    password: "welcome123",
+    user: "dlndhisnezlfir",
+    host: "ec2-54-161-189-150.compute-1.amazonaws.com",
+    password: "97dc6c2cce0633cf9f147f4e3038d106401edd808a33ed18fd008a6d7e59dd0e",
     port: 5432,
-    database: "dinnerheist-gfrontline-db",
+    database: "d9ie64bk2fabqo",
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
