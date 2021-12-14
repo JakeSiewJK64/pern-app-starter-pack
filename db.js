a92da0dc5b9347dbcaaa25531cc8ea7349366ceb
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-14 16:13:19 +0800

    updated db config and added env file

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
+require('dotenv').config();
+
 const pool = new Pool({
-    user: "dlndhisnezlfir",
-    host: "ec2-54-161-189-150.compute-1.amazonaws.com",
-    password: "97dc6c2cce0633cf9f147f4e3038d106401edd808a33ed18fd008a6d7e59dd0e",
-    port: 5432,
-    database: "d9ie64bk2fabqo",
-    ssl: {
-        rejectUnauthorized: false
-    }
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = pool;
