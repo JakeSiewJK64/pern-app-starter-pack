1259497b497c7481fff081f209c703b178d6cfec
Siew Joe Kane <joekanesiew@outlook.com>
2021-12-15 21:00:38 +0800

    update db

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Pool = require("pg").Pool;
-require('dotenv').config();
+require("dotenv").config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -7,6 +7,9 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: parseInt(process.env.DB_PORT),
   database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
